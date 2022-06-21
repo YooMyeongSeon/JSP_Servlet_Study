4f717f5aa73afd40635bf5081d10f6f6db60348d
YooMyeongSeon <s.cnelly@hotmail.com>
2022-06-21 17:59:16 +0900

    2022.06.21 commit

---
+++ web12/src/com/green/ajax/QuizServlet.java
@@ -0,0 +1,45 @@
+package com.green.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+@WebServlet("/QuizServlet")
+public class QuizServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		String getInfo = request.getParameter("Info");
+		String date = "2022.06.21";
+		
+		JsonParser parser = new JsonParser();
+		JsonElement data = parser.parse(getInfo);
+		
+		Quiz_Vo Info = new Quiz_Vo();
+		
+		Info.setNum(Integer.parseInt(data.getAsJsonObject().get("num").getAsString()));
+		Info.setTitle(data.getAsJsonObject().get("title").getAsString());
+		Info.setContent(data.getAsJsonObject().get("content").getAsString());
+		Info.setDate(date);
+		
+		Gson gson = new Gson();
+		
+		String InfoJson = gson.toJson(Info);
+		PrintWriter out = response.getWriter();
+		out.print(InfoJson);
+	}
+}
